633846151be8e46de4e2336d88daf301f2924bc8
omriv88 <113102456+omriv88@users.noreply.github.com>
2023-04-29 01:22:03 +0300

    Update yq_menu.sh

--- yq_menu.sh
+++ yq_menu.sh
@@ -3,8 +3,10 @@
 clear
 # Display Help
 help_menu() {
-  echo "yq - commands"
-  echo "Syntax: ./run.sh <option> <file1.yml> <file2.yml>"
+  echo "Provide at least one argument"
+  echo "Script options:"
+  echo ""
+  echo "Syntax: ./run.sh <option> <file1> <file2>"
   echo "____________________________________________"
   echo "options:"
   echo " merge - Merge the files"
@@ -16,9 +18,9 @@ help_menu() {
 }
 
 
-get_commands_and_files() {
+get_commands_and_yaml_files() {
   read -p "Enter a command: " command
-  if [ "$command" != "merge" ] && [ "$command" != "common" ] && [ "$command" != "unique" ] && [ "$command" != "sort" ]; then
+  if [ "$command" != "merge" ] && [ "$command" != "unique" ] && [ "$command" != "common" ] && [ "$command" != "sort" ]; then
     echo "Error: Invalid command"
     exit 1
   fi
@@ -44,16 +46,35 @@ files_checks(){
 
 yq_command() {
   if [ "$command" = "merge" ]; then
-    yq eval -j "$yaml_file1" "$yaml_file2"
-  elif [ "$command" = "common" ]; then
-    yq eval-all '(.[0] * .[1])' "$yaml_file1" "$yaml_file2"
+  #    yq eval -j "$yaml_file1" "$yaml_file2"
+    yq e "$yaml_file1" "$yaml_file2"
   elif [ "$command" = "unique" ]; then
-    yq 'keys | unique' "$yaml_file1" "$yaml_file2"
+    yq eval -N 'to_entries | unique' "$yaml_file1" "$yaml_file2"
+#    keys=$(yq eval -N 'keys | unique' "$yaml_file1" "$yaml_file2")
+#    value=$(yq ".$key" "$yaml_file1" "$yaml_file2") 
+#    for key in $keys; do
+#    echo $keys
+#    echo "$value"
+#    key_list=$(yq eval -N 'keys | unique' "$yaml_file1" "$yaml_file2") 
+#    echo $key_list | sed 's/-*//g'
+#    echo $key_list | sed 's/^[ \t]*//'
+#    echo $key_list
+#    for yq_key in $key_list;do
+#    clean_key=$(echo $key_list | sed 's/- *//g')
+#    for key in $clean_key; do
+#    echo $word
+#    echo $clean_key
+#    echo $line
+#    yq_r=$(yq ".$key" "$yaml_file1" "$yaml_file2")
+#    echo $yq_r
+#    done
+  elif [ "$command" = "common" ]; then
+    yq eval -N 'to_entries' "$yaml_file1" "$yaml_file2"
   elif [ "$command" = "sort" ]; then
-    yq 'keys' "$yaml_file1" "$yaml_file2"
+    yq eval -N 'keys' "$yaml_file1" "$yaml_file2"
   fi
 }
 help_menu
-get_commands_and_files
+get_commands_and_yaml_files
 files_checks
 yq_command
